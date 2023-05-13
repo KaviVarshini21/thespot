30ab47dcde533d9b4031d82dbae6b360f6426efe
kavi <kavi@kaviMacBook-Pro.local>
2023-05-14 02:59:52 +0530

    final1

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import {AuthProvider} from './context/auth';
 import { SearchProvider } from "./context/search";
 import 'antd/dist/reset.css';
@@ -11,7 +11,7 @@ import {CartProvider} from "./context/cart"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
  
-        <BrowserRouter>
+        <HashRouter>
         <SearchProvider>
           <CartProvider>
         <AuthProvider>
@@ -19,7 +19,7 @@ root.render(
           </AuthProvider>
           </CartProvider>
           </SearchProvider>
-        </BrowserRouter>
+        </HashRouter>
 
 );
 
